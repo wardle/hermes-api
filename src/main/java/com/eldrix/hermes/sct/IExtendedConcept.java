a1ccc7e580ed9eed73212826d9232f583b561f9e
Mark Wardle <mark@wardle.org>
2023-01-25 12:12:03 +0000

    Update IExtendedConcept.java

--- src/main/java/com/eldrix/hermes/sct/IExtendedConcept.java
+++ src/main/java/com/eldrix/hermes/sct/IExtendedConcept.java
@@ -10,7 +10,7 @@ import java.util.Set;
  */
 public interface IExtendedConcept extends IConcept {
     List<IDescription> descriptions();
-    
+
     Map<Long, Set<Long>> parentRelationships();
 
     Map<Long, Set<Long>> directParentRelationships();
